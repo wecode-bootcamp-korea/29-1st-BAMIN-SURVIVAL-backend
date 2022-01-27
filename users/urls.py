2d8dce056404385fb4b942d74f01769bfdf34f8a
sosolyht <sosolyht@gmail.com>
2022-01-27 17:23:48 +0900

    [add] update signup & signin features

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from users.views import SignInView, SignUpView
+
 urlpatterns = [
+    path('/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view()),
 ]
---
+++ users/views.py
@@ -0,0 +1,66 @@
+import json
+import bcrypt
+import jwt
+
+from django.http     import JsonResponse
+from django.views    import View
+from django.conf     import settings
+
+from users.models    import User
+from users.validator import email_validation, password_validation
+
+class SignUpView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        try:
+            account       = data['account']
+            nickname      = data['nickname']
+            password      = data['password']
+            email         = data['email']
+            phone         = data['phone']
+
+            email_validation(email)
+            password_validation(password)
+
+            if User.objects.filter(account=account).exists():
+                return JsonResponse({'message':'ACCOUNT ALREADY EXISTS'}, status = 400)
+            if User.objects.filter(nickname=nickname).exists():
+                return JsonResponse({'message':'NICKNAME ALREADY EXISTS'}, status = 400)
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message':'E-MAIL ALREADY EXISTS'}, status = 400)
+            if User.objects.filter(phone = phone).exists():
+                return JsonResponse({'message':'PHONE-NUMBER ALREADY EXISTS'}, status=400)
+
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            User.objects.create(
+                account     = account,
+                nickname    = nickname,
+                password    = hashed_password,
+                email       = email,
+                phone       = phone,
+                point       = 100000
+            ) 
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+            
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+class SignInView(View):
+    def post(self,request):
+        try:
+            data     = json.loads(request.body)
+            account  = data['account']
+            password = data['password']
+            user = User.objects.get(account = account)
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+            token = jwt.encode({'users_id': user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+            return JsonResponse({'message' : 'SUCCESS', 'JWT' : token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'DOES NOT EXIST USER'}, status = 400)
