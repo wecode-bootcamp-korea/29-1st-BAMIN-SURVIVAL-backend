e5aefccfa74133a4223f0af0cf85d25b3a7104ce
sosolyht <sosolyht@gmail.com>
2022-01-27 13:44:16 +0900

    [add] users on orders & fix migrations

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-from core.models import Base
+from core.models  import Base
+from users.models import User
 
 class Order(Base):
     order_number = models.IntegerField()
+    users = models.ForeignKey(User, on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'orders'
