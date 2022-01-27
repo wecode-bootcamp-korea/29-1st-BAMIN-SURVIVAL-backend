e2145188138ea65bf1a53ef23921bb7a9078547c
sosolyht <sosolyht@gmail.com>
2022-01-27 20:44:28 +0900

    [WIP] update models
    친환경 소재인 green 과 세일상품 sale 추가, size 옵션 테이블 따로 구현
    
    [add] discount rate & discount price models
    
    [fix] rename url to img_url & conventions
    
    [add] migrations files

--- products/models.py
+++ products/models.py
@@ -10,26 +10,33 @@ class Category(Base):
 
 class ProductOption(Base):
     product = models.ForeignKey('Product', on_delete = models.CASCADE)
-    price   = models.PositiveIntegerField()
-    size    = models.CharField(max_length = 5)
-    color   = models.CharField(max_length = 10)
-    option  = models.CharField(max_length = 30)
-    agency  = models.CharField(max_length = 10)
+    size    = models.ForeignKey('Size', on_delete = models.CASCADE, null=True)
 
     class Meta:
         db_table = 'product_options'
 
 class Product(Base):
-    category = models.ForeignKey('Category', on_delete = models.CASCADE)
-    name     = models.CharField(max_length = 50)
-    shipping = models.CharField(max_length = 10)
+    category       = models.ForeignKey('Category', on_delete = models.CASCADE)
+    name           = models.CharField(max_length = 50)
+    shipping       = models.CharField(max_length = 10)
+    price          = models.PositiveIntegerField()
+    discount_rate  = models.PositiveSmallIntegerField(null = True)
+    discount_price = models.PositiveIntegerField(null = True)
+    is_green       = models.BooleanField(default = False)
+    is_sale        = models.BooleanField(default = False)
 
     class Meta:
         db_table = 'products'
 
 class Image(Base):
-    url     = models.URLField(max_length = 200)
+    img_url = models.URLField(max_length = 200)
     product = models.ForeignKey('Product', on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'images'
+
+class Size(models.Model):
+    name = models.CharField(max_length = 5)
+
+    class Meta:
+        db_table = 'sizes'
