302bb20f227fe2b6d1546d49130c20972a233f54
sosolyht <sosolyht@gmail.com>
2022-01-30 01:44:00 +0900

    [WIP] add product views
    [WIP] add product detail views
    
    [WIP] add category views
    
    test
    
    Revert "test"
    
    This reverts commit 1335fd11a905cef2dc6161c1abb66d7cb019704c.
    
    fix conventions
    
    remove white space
    
    remove white space
    
    [fix] add image on product detail view
    
    [fix] comprehension
    
    [fix] 500 error
    
    [fix] good logic
    
    [fix] details

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls    import path
+
+from products.views import ProductDetailView, CategoryView, ProductAllView
+
+urlpatterns = [
+    path("/<int:product_id>",ProductDetailView.as_view()),
+    path("/<str:category_name>", CategoryView.as_view()),
+    path("/all",ProductAllView.as_view())
+]
