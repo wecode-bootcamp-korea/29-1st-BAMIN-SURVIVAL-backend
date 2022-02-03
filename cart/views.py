67c03a5de2ed4194d840630aa45d760d27b54ceb
sosolyht <sosolyht@gmail.com>
2022-02-03 22:58:14 +0900

    [WIP] add cart views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+class CartView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
