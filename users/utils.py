48035321404ce3582cb5ed374b5f640ff30ea1e1
sosolyht <sosolyht@gmail.com>
2022-02-05 00:10:16 +0900

    [add] login user decortor
    [fix] remove urls
    
    [modify] change jwt token message
    
    [add] unauthorized error handling

---
+++ users/utils.py
@@ -0,0 +1,25 @@
+import jwt
+
+from django.conf  import settings
+from django.http  import JsonResponse
+
+from users.models import User
+
+def login_required(func):
+    def wrapper(self, request, *args, **kwargs):
+        if 'Authorization' not in request.headers:
+            return JsonResponse ({'message' : 'UNAUTHORIZED'}, status=401)
+        try:
+            access_token = request.headers.get('Authorization')
+            payload  = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+            request.user = User.objects.get(id = payload['user_id'])
+        
+        except jwt.DecodeError:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status = 401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'USER_DOES_NOT_EXIST'}, status = 400)
+
+        return func(self, request, *args, **kwargs)   
+
+    return wrapper
