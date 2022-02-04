17ef2f6eaad6bd3ceea038eec16a18cf31257f4e
sosolyht <sosolyht@gmail.com>
2022-02-04 16:31:17 +0900

    WIP

--- cart/views.py
+++ cart/views.py
@@ -3,9 +3,13 @@ import json
 from django.views import View
 from django.http import JsonResponse
 
+from cart.models import Cart
+from users.models import User
+
 class CartView(View):
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-        except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+    def post(self, request): 
+        data       = json.loads(request.body)
+        user           = User.objects.get(id=request.users.id)
+        quantity   = data['quantity']
+        print(user)
+        print(quantity)
