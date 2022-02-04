df828fc06c04f54de0982b0b64e8f1bbdd58ec3b
sosolyht <sosolyht@gmail.com>
2022-02-04 23:51:49 +0900

    [fix] decorator
    [fix] decorator

--- users/utils.py
+++ users/utils.py
@@ -8,11 +8,9 @@ from users.models import User
 def login_required(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            access_token = request.header.get('Authorization')
+            access_token = request.headers.get('Authorization')
             payload  = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
-            login_users = User.objects.get(id=payload['id'])
-            request.users = login_users
-            users_id = login_users.id
+            request.user = User.objects.get(id = payload['users_id'])
         
         except jwt.DecodeError:
             return JsonResponse({'message': 'INVALID_TOKEN'}, status = 401)
