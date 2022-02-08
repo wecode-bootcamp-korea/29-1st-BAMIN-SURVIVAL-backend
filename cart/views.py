fea63afb44f9feff65f842cacd1ad01af5b95d0b
HoJun Jang <wkdghwns91@gmail.com>
2022-02-08 15:32:10 +0900

    live-code-review

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,132 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+
+from cart.models     import Cart
+from users.models    import User
+from products.models import ProductOption
+from users.utils     import login_required
+
+# product option 도 넣어줘야함.
+class CartView(View):
+    @login_required
+    def post (self, request):
+        try:
+            data              = json.loads(request.body)
+            user              = request.user
+            quantity          = data['quantity']
+            product_option_id = data['product_option_id']
+
+            cart, created     = Cart.objects.get_or_create(
+                user              = user,
+                product_option_id = product_option_id,
+                defaults = {}
+            )
+
+            if not created:
+                cart.quantity += quantity
+                cart.save()
+
+#             cart = Cart.objects.create(
+#                 users_id     = request.user.id,
+#                 products_id  = data['products_id']
+#             )
+            # quantity 수량 연산 식 써줘야합니다
+
+            current_total_cart = Cart.objects.filter(users_id=request.user.id).count()
+
+            success_message = {
+                'result' : {
+                    'message' : 'SUCCESS',
+                    'current_total_cart': current_total_cart
+                }
+            }
+            # users_id 가 생기면 카운트
+            # 임시 메시지
+            return JsonResponse(success_message, status = 200)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'USER_ID_DOES_NOT_EXIST'}, status = 400)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)               
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)         
+
+    @login_required
+    def get (self, request):
+        # products 조회 후 quantity 도 넣어줘야합니다.
+        # 수정 중
+        user   = request.user
+        carts  = Cart.objects.filter(user = user)
+        count  = carts.count()
+
+        result = [
+            {
+                'name'           : cart.product.name,
+                'price'          : item.products.price,
+                #'options'        : #옵션 넣어야함
+                'quantity'       : item.quantity,
+                'shipping'       : item.products.shipping,
+                'thumbnail_img'  : item.products.image_set.all()[0].img_url
+            } for cart in carts ]
+        return JsonResponse({
+            'result' : result,
+            'count'  : count
+        }, status = 200)
+
+    @login_required
+    def patch (self, request):
+        try:
+            data = json.loads(request.body)
+            cart = Cart.objects.get(id = data['cart_id'])
+            #옵션 들어가야함
+            cart.quantity = data['quantity']
+            cart.save()
+
+            current_total_cart = Cart.objects.filter(users_id=request.user.id).count()
+            success_message = {
+                'result' : {
+                    'message' : 'SUCCESS',
+                    'current_total_cart': current_total_cart
+                }
+            }
+            return JsonResponse(success_message, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)
+
+    @login_required
+    def delete (self, request):
+        try:
+            # :8000/cart?cart_id=1&cart_id=2&cart_id=3
+            cart_ids = request.GET.getlist(cart_id, None)
+            # cart_ids = [1, 2, 3]
+
+            carts = Cart.objects.filter(id__in=cart_ids)
+
+            if not carts:
+                return JsonResponse({'message' : 'CART DOES NOT EXIST'})
+
+            carts.delete()
+
+#             data = json.loads(request.body)
+#             Cart.objects.get(id = data['cart_id']).delete()
+            #옵션 들어가야하는지.
+#             current_total_cart = Cart.objects.filter(users_id=request.user.id).count()
+            success_message = {
+                'result' : {
+                    'message' : 'SUCCESS'
+#                     'current_total_cart': current_total_cart
+                }
+            }
+            return JsonResponse(success_message, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)      
+        except Cart.DoesNotExist:
+            return JsonResponse({'message' : 'CART_DOSE_NOT_EXIST'}, status = 400)
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)
--- products/views.py
+++ products/views.py
@@ -1,28 +1,50 @@
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
 from products.models import Product, Category
 
-class ProductAllView(View):
+class ProductListView(View):
     def get(self, request):
         try:
-            alls = Product.objects.all()
-            result = [
-                {
-                    'id'             : all.id,
-                    'name'           : all.name,
-                    'shipping'       : all.shipping,
-                    'price'          : all.price,
-                    'is_green'       : all.is_green,
-                    'is_sale'        : all.is_sale,
-                    'category_id'    : all.category_id,
-                    'stock'          : all.stock,
-                    'discount_price' : all.discount_price,
-                    'thumbnail_image': [image.img_url for image in all.image_set.all()]
-                    }for all in alls]
+            # QueryDict
+            # :8000/product?limit=20&offset=0
+            limit       = request.GET.get('limit', 20)
+            offset      = request.GET.get('offset', 0)
+            category = int(request.GET.get('category', None))
+            sort        = request.GET.get('sort', '-created_at')
 
-            return JsonResponse({'result': result}, status = 200) 
-        
+            q = Q()
+
+            if category:
+                q &= Q(category__name = category)
+
+            products = Product.objects.filter(q)\
+                                      .order_by(sort)[offset : offset+limit]
+
+            result = [{
+
+            } for product in products ]
+
+            return JsonResponse({'message' : result}, status=200)
+
+#             alls = Product.objects.all()
+#             result = [
+#                 {
+#                     'id'             : all.id,
+#                     'name'           : all.name,
+#                     'shipping'       : all.shipping,
+#                     'price'          : all.price,
+#                     'is_green'       : all.is_green,
+#                     'is_sale'        : all.is_sale,
+#                     'category_id'    : all.category_id,
+#                     'stock'          : all.stock,
+#                     'discount_price' : all.discount_price,
+#                     'thumbnail_image': [image.img_url for image in all.image_set.all()]
+#                     }for all in alls]
+# 
+#             return JsonResponse({'result': result}, status = 200) 
+#         
         except Product.DoesNotExist:
             return JsonResponse({'message' : 'PRODUCT_DOES_NOT_EXIST'}, status = 400)
 
@@ -32,35 +54,47 @@ class ProductAllView(View):
         except TypeError:
             return JsonResponse({'message' : 'TYPE_ERROR'}, status = 400)
 
-class CategoryView(View):
-    def get(self, request, category_name):
-        try:
-            category_name = Category.objects.get(name = category_name)
-            category_data = Product.objects.filter(category_id = category_name.id)
-            result = [
-                {
-                    'id'             : category_item.id,
-                    'name'           : category_item.name,
-                    'shipping'       : category_item.shipping,
-                    'price'          : category_item.price,
-                    'is_green'       : category_item.is_green,
-                    'is_sale'        : category_item.is_sale,
-                    'category_id'    : category_item.category_id,
-                    'stock'          : category_item.stock,
-                    'discount_price' : category_item.discount_price,
-                    'thumbnail_image': [image.img_url for image in category_item.image_set.all()]
-                    }for category_item in category_data]
+class CategoryListView(View):
+    # :8000/products/categories
+    def get(self, request):
+        categories = Category.objects.all()
 
-            return JsonResponse({'result' : result}, status = 200)
+        results = [{
+            'id'   : category.id,
+            'name' : category.name
+        } for category in categories]
 
-        except Category.DoesNotExist:
-            return JsonResponse({'message' : 'CATEGORY_DOES_NOT_EXIST'}, status = 400)
+        return JsonResponse({...})
 
-        except AttributeError:
-            return JsonResponse({'message' : 'ATTRIBUTE_ERROR'}, status = 400)
-        
-        except TypeError:
-            return JsonResponse({'message' : 'TYPE_ERROR'}, status = 400)   
+# class CategoryView(View):
+#     def get(self, request, category_name):
+#         try:
+#             category_name = Category.objects.get(name = category_name)
+#             category_data = Product.objects.filter(category_id = category_name.id)
+#             result = [
+#                 {
+#                     'id'             : category_item.id,
+#                     'name'           : category_item.name,
+#                     'shipping'       : category_item.shipping,
+#                     'price'          : category_item.price,
+#                     'is_green'       : category_item.is_green,
+#                     'is_sale'        : category_item.is_sale,
+#                     'category_id'    : category_item.category_id,
+#                     'stock'          : category_item.stock,
+#                     'discount_price' : category_item.discount_price,
+#                     'thumbnail_image': [image.img_url for image in category_item.image_set.all()]
+#                     }for category_item in category_data]
+# 
+#             return JsonResponse({'result' : result}, status = 200)
+# 
+#         except Category.DoesNotExist:
+#             return JsonResponse({'message' : 'CATEGORY_DOES_NOT_EXIST'}, status = 400)
+# 
+#         except AttributeError:
+#             return JsonResponse({'message' : 'ATTRIBUTE_ERROR'}, status = 400)
+#         
+#         except TypeError:
+#             return JsonResponse({'message' : 'TYPE_ERROR'}, status = 400)   
 
 class ProductDetailView(View):
     def get(self, request, product_id):
